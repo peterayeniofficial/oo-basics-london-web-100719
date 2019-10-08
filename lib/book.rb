91971108e4774fecca51661999150929610a522a
Peter Ayeni <ayenicology@gmail.com>
2019-10-08 15:37:28 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,5 +1,7 @@
 class Book
   
+  attr_accessor :author, :page_count, :genre
+  
   def initialize(title)
     @title = title
   end
@@ -10,26 +12,6 @@ class Book
   
   def author=(author)
     @author = author
-  end
-  
-  def author
-    @author
-  end
-  
-  def page_count=(num)
-    @page = num
-  end
-  
-  def page_count
-    @page_count
-  end
-  
-  def genre=(genre)
-    @genre = genre
-  end
-  
-  def genre
-    @genre
   end
   
   def turn_page
